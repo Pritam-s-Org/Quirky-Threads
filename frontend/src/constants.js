8b668b4af974a3db24a569e8570d40a08634609c
pritam19-d <125026007+pritam19-d@users.noreply.github.com>
2025-09-27 02:07:32 +0530

    Refactor: Update BASE_URL to use the correct production endpoint for deployment

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = process.env.NODE_ENV === "development" ? "http://localhost:5000" : "https://quirky-threads.onrender.com"
+export const BASE_URL = process.env.NODE_ENV === "development" ? "http://localhost:5000" : "https://quirky-threads.duckdns.org"
 // export const BASE_URL = "";
 export const PRODUCTS_URL = "/api/products";
 export const USERS_URL = "/api/users";
