6c0a772b079fb5be6d324a2044b519203d4a610d
pritam19-d <125026007+pritam19-d@users.noreply.github.com>
2025-10-14 00:56:22 +0530

    Refactor: Add hover animation to Product card for enhanced interactivity

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -6,7 +6,7 @@ import { BASE_URL } from "../constants"
 
 const Product = ( {product} ) => {
   return (
-    <Card className="my-3 p-3 rounded fixed-height">
+    <Card className="my-3 p-3 rounded fixed-height hover-popup-animation">
       <Link to={`/product/${product._id}`} className="position-relative d-inline-block">
         <Card.Img src={BASE_URL + product?.variants[0]?.images[0]} variant="top" style={{height:"16rem", objectFit: "cover"}} />
         {product.totalInStock <= 0 && <div className="position-absolute top-0 start-0 w-100 h-100 bg-dark bg-opacity-50" />}
